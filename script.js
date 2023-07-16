b9d8781c7c7d01ea147b762c8d14424dc01e7be8
Vivek Kushwaha <52906440+vikdevjs@users.noreply.github.com>
2023-07-16 19:36:49 +0530

    Added count

--- script.js
+++ script.js
@@ -86,3 +86,14 @@ function success() {
   function fetchagain(){
       window.location.reload();
   }
+
+const countEl = document.getElementById("count");
+      countvisits();
+
+      function countvisits() {
+        fetch('https://api.countapi.xyz/update/laptop/mouse/?amount=1')
+          .then((res) => res.json())
+          .then((res) => {
+            countEl.innerHTML = res.value;
+          });
+      }
